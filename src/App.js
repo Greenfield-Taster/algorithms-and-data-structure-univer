71bc7e325dc5d509f4a5b20fd1e68839591ae8b5
Anastasiia Horbachova <77613323+Greenfield-Taster@users.noreply.github.com>
2024-09-23 20:11:50 +0300

    Add styles to the main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
+import "./App.css";
 import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Link to="/project-1">move to proj 1</Link>
+      <header>Univer projects developed by Anastasiia Horbachova</header>
+      <div className="wrapper">
+        <div className="blocks">
+          <Link to="/project-1" className="link">
+            <div className="block">Move to project 1</div>
+          </Link>
+          <Link to="/project-1" className="link">
+            <div className="block">Move to project 2</div>
+          </Link>
+          <Link to="/project-1" className="link">
+            <div className="block">Move to project 3</div>
+          </Link>
+          <Link to="/project-1" className="link">
+            <div className="block">Move to project 4</div>
+          </Link>
+          <Link to="/project-1" className="link">
+            <div className="block">Move to project 5</div>
+          </Link>
+          <Link to="/project-1" className="link">
+            <div className="block">Move to project 6</div>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }

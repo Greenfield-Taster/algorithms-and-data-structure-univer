87dcd3843ea39a1d5da51598427dfcd1d94d4819
Anastasiia Horbachova <77613323+Greenfield-Taster@users.noreply.github.com>
2024-09-26 12:48:35 +0300

    Add heap class and methods

--- src/projects/project-1/Project1.jsx
+++ src/projects/project-1/Project1.jsx
@@ -7,7 +7,7 @@ import {Task3} from "./components/Task3.js"
 function Project1() {
   const list = new Task1();
 
-  const handleActions = () => {
+  const handleActionsList = () => {
     list.addToStart(1);
     list.addToStart(2);
     list.addAfter(1, 3);
@@ -17,14 +17,33 @@ function Project1() {
     list.displayFromEnd();
   };
 
+  const heap = new Task2();
+
+ const handleActionsHeap =()=>{
+   heap.insert(10);
+   heap.insert(5);
+   heap.insert(20);
+   heap.insert(1);
+   heap.display();
+
+   heap.remove();
+   heap.display();
+
+   const arr = [3, 9, 2, 1, 7, 8];
+   heap.buildHeap(arr);
+   heap.display();
+
+   heap.heapSort(); 
+ } 
+
   return (
     <div className="Project1">
       <div className="wrapperProj1">
         <div className="blockProj1">
-          <button onClick={handleActions}>Двозв'язний список</button>
+          <button onClick={handleActionsList}>Двозв'язний список</button>
         </div>
         <div className="blockProj1">
-          <button onClick={Task2}>Купа</button>
+          <button onClick={handleActionsHeap}>Купа</button>
         </div>
         <div className="blockProj1">
           <button onClick={Task3}>Пірамідальне сортування (варіант 3)</button>
--- src/projects/project-1/components/Task2.js
+++ src/projects/project-1/components/Task2.js
@@ -1,3 +1,128 @@
-export function Task2(){
-    console.log("task2")
+export class Task2 {
+  constructor() {
+    this.heap = [];
+  }
+
+  getParentIndex(i) {
+    return Math.floor((i - 1) / 2);
+  }
+  getLeftChildIndex(i) {
+    return 2 * i + 1;
+  }
+  getRightChildIndex(i) {
+    return 2 * i + 2;
+  }
+
+  swap(index1, index2) {
+    [this.heap[index1], this.heap[index2]] = [
+      this.heap[index2],
+      this.heap[index1],
+    ];
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+    console.log(`Inserted ${value}`);
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (
+      index > 0 &&
+      this.heap[index] > this.heap[this.getParentIndex(index)]
+    ) {
+      this.swap(index, this.getParentIndex(index));
+      index = this.getParentIndex(index);
+    }
+  }
+
+  remove() {
+    if (this.heap.length === 0) {
+      console.log("Heap is empty");
+      return null;
+    }
+
+    const root = this.heap[0];
+    const last = this.heap.pop();
+
+    if (this.heap.length > 0) {
+      this.heap[0] = last;
+      this.heapifyDown();
+    }
+
+    console.log(`Removed ${root}`);
+    return root;
+  }
+
+  heapifyDown() {
+    let index = 0;
+    const length = this.heap.length;
+
+    while (this.getLeftChildIndex(index) < length) {
+      let largerChildIndex = this.getLeftChildIndex(index);
+
+      if (
+        this.getRightChildIndex(index) < length &&
+        this.heap[this.getRightChildIndex(index)] > this.heap[largerChildIndex]
+      ) {
+        largerChildIndex = this.getRightChildIndex(index);
+      }
+
+      if (this.heap[index] >= this.heap[largerChildIndex]) {
+        break;
+      }
+
+      this.swap(index, largerChildIndex);
+      index = largerChildIndex;
+    }
+  }
+
+  buildHeap(array) {
+    this.heap = array;
+    // Применяем heapifyDown начиная с родителя последнего элемента
+    for (let i = Math.floor(array.length / 2) - 1; i >= 0; i--) {
+      this.heapifyDownAt(i);
+    }
+    console.log("Heap built from array:", this.heap);
+  }
+
+  heapifyDownAt(index) {
+    const length = this.heap.length;
+
+    while (this.getLeftChildIndex(index) < length) {
+      let largerChildIndex = this.getLeftChildIndex(index);
+
+      if (
+        this.getRightChildIndex(index) < length &&
+        this.heap[this.getRightChildIndex(index)] > this.heap[largerChildIndex]
+      ) {
+        largerChildIndex = this.getRightChildIndex(index);
+      }
+
+      if (this.heap[index] >= this.heap[largerChildIndex]) {
+        break;
+      }
+
+      this.swap(index, largerChildIndex);
+      index = largerChildIndex;
+    }
+  }
+
+  heapSort() {
+    const sortedArray = [];
+    const originalHeap = [...this.heap];
+
+    while (this.heap.length > 0) {
+      sortedArray.push(this.remove());
+    }
+
+    this.heap = originalHeap;  
+    console.log("Sorted array:", sortedArray);
+    return sortedArray;
+  }
+
+  display() {
+    console.log("Heap elements:", this.heap);
+  }
 }
